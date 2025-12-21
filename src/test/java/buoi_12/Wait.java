3e001ca18d7824b1f19b3811bcabeff9d4c8d560
GiangQueMan <giangqueman@gmail.com>
2025-12-21 13:56:40 +0700

    selenium wait

---
+++ src/test/java/buoi_12/Wait.java
@@ -0,0 +1,52 @@
+package buoi_12;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Wait {
+    static WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // implcit wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    }
+
+    @Test
+    public void customDropdown() {
+        // explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // đợi đến khi tìm được xpath username và nhập username
+        WebElement username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='username']")));
+        username.sendKeys("Admin");
+
+        // đợi đến khi tìm được xpath password và nhập password
+        WebElement password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='password']")));
+        password.sendKeys("admin123");
+
+        // đợi đến khi tìm được button login và click
+        WebElement loginButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
+        loginButton.click();
+    }
+    @AfterClass
+    public static void tearDown(){
+        driver.quit();
+    }
+}
